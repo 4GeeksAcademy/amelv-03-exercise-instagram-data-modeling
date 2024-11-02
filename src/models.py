877996109721747c1cbd9808e83f17936de32939
amelv-03 <aiden.melv03@gmail.com>
2024-11-02 00:22:21 +0000

    dasdasdasdad

--- src/models.py
+++ src/models.py
@@ -7,15 +7,15 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Follower(Base):
+    __tablename__ = 'follower'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class User(Base):
+    __tablename__ = 'User'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
